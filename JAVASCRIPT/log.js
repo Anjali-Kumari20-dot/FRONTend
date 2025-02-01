51f4ebee43d59b2b5c15493c513dd07c023c34c1
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-01 20:56:16 +0530

    Basics

---
+++ JAVASCRIPT/log.js
@@ -0,0 +1,9 @@
+// console.log("Hello heading towards full stack development course");
+// console.log("Apna", "College", 123);
+
+let pencilPrice = 10;
+let erasorPrice = 5;
+// let output = console.log("The total price is : ",pencilPrice + erasorPrice, "Rupees. ");
+
+let output = `The total price is: ${pencilPrice + erasorPrice} Rupees.`;
+console.log(output);
