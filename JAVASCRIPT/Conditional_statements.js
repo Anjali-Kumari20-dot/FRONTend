59055f257cf3961efe7d0a353fcdc52f0a58d278
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-02 12:37:31 +0530

    Conditionals Statements

---
+++ JAVASCRIPT/Conditional_statements.js
@@ -0,0 +1,41 @@
+// console.log("Before my if statements");
+// let age = 23;
+// if(age >= 18){
+//     console.log("You can vote.");
+//     console.log("You can drive.");
+//     console.log("You can choose your partner.");
+// }
+// console.log("After my if statements");
+
+// let firstName = "Anjali";
+// if(firstName = "Anjali"){
+//     console.log(`Welcome ${firstName}`);
+// }
+
+// let color = "yellow";
+// if(color == "red"){
+//     console.log("STOP!");
+// }
+// else if(color == "green"){
+//         console.log("GO");
+//     }
+//     else if(color == "yellow"){
+//         console.log("SLOW DOWN");
+//     }
+//         else {
+//             console.log("IGNORE");
+//         }
+
+let marks = 75;
+if(marks >= 80){
+    console.log("A+");
+}
+else if(marks >= 60){
+    console.log("A");
+}
+else if(marks >= 33){
+    console.log("B");
+}
+else if(marks < 33 ){
+    console.log("F");
+}
