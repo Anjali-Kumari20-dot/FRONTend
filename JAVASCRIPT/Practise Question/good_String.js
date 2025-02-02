1b005c8a4e80e1f3215ede2ac1a30cb96f16718d
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-02 14:01:18 +0530

    truthy and Falsy

---
+++ JAVASCRIPT/Practise Question/good_String.js
@@ -0,0 +1,20 @@
+let str = "ap";
+if(str.length > 3 && str[0] == 'a'){
+    console.log("good");
+}else{
+    console.log("not good");
+}
+
+let string = " ";
+if(string){
+    console.log("String is not empty");
+} else {
+    console.log("String is empty");
+}
+
+let num = -10;
+if(num) {
+    console.log("num is not equal to 0");
+}else {
+    console.log("num is equal to 0");
+}
