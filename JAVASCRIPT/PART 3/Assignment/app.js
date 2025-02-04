895baa6b9d81820bcb5d76d622d8ba6e55ce05d1
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-04 18:32:59 +0530

    Assignment

---
+++ JAVASCRIPT/PART 3/Assignment/app.js
@@ -0,0 +1,47 @@
+// QUESTION 1
+let arr = [1, 2, 3, 4, 5, 6, 7];
+let n1 = 3;
+
+let ans1 = arr.splice(0, n1);
+console.log(ans1);
+
+// QUESTION 2
+// let brr = [1, 2, 3, 4, 5, 6, 7];
+// let n2 = 3
+
+// let ans2 = brr.splice(brr.length - n2);
+// console.log(ans2);
+
+// QUESTION 3
+// let str1 = prompt("Please enter a string");
+
+// if(str1.length == 0){
+//     console.log("String is empty");
+// }else {
+//     console.log("String is not empty");
+// }
+
+// QUESTION 4
+// let str2 = "ApNaCoLlEgE";
+// let idx = 3;
+
+// if(str2[idx] == str2[idx].toLowerCase()) {
+//     console.log("Characters is lowerCase");
+// }else {
+//     console.log("Character is not lowerCase");
+// }
+
+// QUESTION 5
+// let str3 = prompt("Please enter a string");
+// console.log(`Original string = ${str3}`);
+// console.log(`String without spaces = ${str3.trim()}`);
+
+// QUESTION 6
+// let crr = ["Hello", 'a', 23, 34, 45, 56, -67];
+// let item = 64;
+
+// if(crr.indexOf(item) != -1){
+//     console.log("Elements exists in array");
+// }else {
+//     console.log("Element doesn't exist in array");
+// }
