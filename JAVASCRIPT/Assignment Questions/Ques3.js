2705e2e24e1835ed9ee65eadcaa7f117bc34256d
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-04 09:09:08 +0530

    Strings

--- JAVASCRIPT/Assignment Questions/Ques3.js
+++ JAVASCRIPT/Assignment Questions/Ques3.js
@@ -1,15 +1,16 @@
 let month = prompt("Please enter the quarter month: ");
 switch(month){
-    case "month" == 1 : 
-        alert(`Months in Querter 1 : January, February, March`);
+    case 1 : 
+        console.log(`Months in Quarter 1 : January, February, March`);
         break;
-    case "month" ==  2 : 
-        alert(`Months in Querter 2 : April, May, June`);
+    case 2 : 
+        console.log(`Months in Quarter 2 : April, May, June`);
         break;
-    case "month" ==  3 : 
-        alert(`Months in Querter 3 : July,August,September`);
+    case 3 : 
+        console.log(`Months in Quarter 3 : July,August,September`);
         break;
-    case "month" ==  4 : 
-        alert(`Months in Querter 4 : October,November,December`);
+    case 4 : 
+        console.log(`Months in Quarter 4 : October,November,December`);
         break;
+    default : console.log("NOT A QUARTER!");
 }
---
+++ JAVASCRIPT/PART 3/app.js
@@ -0,0 +1,9 @@
+let name = "    I Love Coding";
+console.log("name.lowerCase() : ", name.toLowerCase());
+console.log("name.toUpperCase() : ", name.toUpperCase());
+console.log(name.indexOf("Love")) // 1
+console.log(name.toUpperCase().trim());
+console.log(name.slice(3,8))
+console.log(name.slice(-5))
+console.log(name.replace("Love", "Do"));
+console.log(name.repeat(3));
