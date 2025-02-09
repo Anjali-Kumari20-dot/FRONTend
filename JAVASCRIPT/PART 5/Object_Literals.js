ed2f718a7c82b6ec4402d3726866831362873320
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-09 07:37:21 +0530

    Object literal basics

---
+++ JAVASCRIPT/PART 5/Object_Literals.js
@@ -0,0 +1,30 @@
+// let arr = [];
+// console.log(typeof arr);
+
+// Object literals are used to store keyed collections and complex entities.
+// propert => (key, value)pair , objects are a collection of properties
+
+const student = {
+    name : "Anjali",
+    age : 20,
+    marks: 94.4
+};
+
+const item = {
+    price: 100.99,
+    discount: 50,
+    colors : ["red", "pink"]
+};
+
+const post = {
+    username: "Anjali",
+    content: "Learning full stack development",
+    likes: 100 + "k",
+    repost: 78,
+    tags: ["Full Stack Development", "Learning", "Coding"]
+};
+
+// GET VALUES
+
+student["name"]
+student.name
