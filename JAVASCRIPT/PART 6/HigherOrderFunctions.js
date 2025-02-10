25728377cc5bda8998eda694eb78d6b6e81f6e68
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-10 13:38:41 +0530

    Higher order functions

---
+++ JAVASCRIPT/PART 6/HigherOrderFunctions.js
@@ -0,0 +1,19 @@
+// HIGHER ORDER FUNCTIONS
+
+// A function that does one or both:
+// -> takes one or multiple functions as arguments
+// -> returns a function
+
+function multipleGreet(func, count){
+    for(let i = 1; i <= count; i++){
+        func();
+    }
+}
+
+let greet = function(){
+    console.log("Hello");
+}
+
+// multipleGreet(function() {console.log("namaste")}, 1000);
+
+multipleGreet(greet(), 1000);
