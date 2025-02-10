329bca3c943c4cea0be5e376af86a10903114fb7
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-10 07:14:24 +0530

    Basics

---
+++ JAVASCRIPT/PART 6/functions_with_Arguments.js
@@ -0,0 +1,18 @@
+function printInfo(name, age){
+    console.log(`${name}'s age is ${age}.`);
+}
+printInfo("Anjali", 20);
+printInfo("Chandan", 21);
+
+function sum(a, b){
+    console.log(`Sum: ${a+b}`);
+}
+
+sum(2856,3456);
+
+function calc_avg(num1, num2, num3){
+    avg = (num1+num2+num3)/3;
+    console.log(`Average of ${num1}, ${num2} & ${num3} is: ${avg}`)
+}
+
+calc_avg(3,3,3);
