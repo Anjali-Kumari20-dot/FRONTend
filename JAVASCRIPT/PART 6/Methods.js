e0cd3fe50e6c4234bee8b222d4ae0592c008aaf2
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-10 14:30:53 +0530

    Methods

---
+++ JAVASCRIPT/PART 6/Methods.js
@@ -0,0 +1,13 @@
+// Methods -> Actions that can be performed on an object.
+
+const calculator = {
+    add: function(a, b){
+        return a + b;
+    },
+    sub: function(a, b){
+        return a - b;
+    },
+    mul: function(a, b){
+        return a * b;
+    }
+}
