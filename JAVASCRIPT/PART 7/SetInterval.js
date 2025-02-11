48d1b2d703fb2f068376ef8e4568f5eac9f1a44a
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-11 22:02:48 +0530

    Set Interval

---
+++ JAVASCRIPT/PART 7/SetInterval.js
@@ -0,0 +1,16 @@
+// SET INTERVAL 
+// setinterval(function, timeout)
+
+// setInterval( () => {
+//     console.log("Apna College");
+// }, 2000);
+
+let id = setInterval(() => {
+    console.log("Apna College");
+}, 2000);
+
+console.log(id);
+
+let id2 = setInterval(() => {
+    console.log("Hello world");
+}, 3000);
