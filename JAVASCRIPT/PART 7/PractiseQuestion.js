5701fe86dd2826fa062382423ef6c2d5350d1ce4
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-12 09:54:56 +0530

    Practise question

---
+++ JAVASCRIPT/PART 7/PractiseQuestion.js
@@ -0,0 +1,5 @@
+// Write an arrow function that returns the square of a number 'n'.
+const square = (n) => n * n;
+
+console.log(square(4));
+// Write a function that prints "Hello World" 5 times at intervals of 2s each.
