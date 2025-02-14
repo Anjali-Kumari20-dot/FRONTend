8afe75170c808cda3d206be27185e9e83ca5eb94
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-14 11:04:32 +0530

    Spread Array Literals

--- JAVASCRIPT/PART 8/Spread.js
+++ JAVASCRIPT/PART 8/Spread.js
@@ -6,4 +6,11 @@ function func(...arr){
 }
 console.log(..."apnacollege");
 let nums = [12,23,34,45,67768,89,5,3,4,445,678,990];
+let newArr = [...nums];
+console.log(newArr);
 console.log(Math.min(...nums));
+
+let odd = [1, 3, 5, 7, 9, 11, 13, 15];
+let even = [2, 4, 6, 8, 10, 12, 14];
+let number = [...odd, ...even];
+console.log(number);
