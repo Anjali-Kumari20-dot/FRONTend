8b1514d0efd43259568419e0cbaf059bceedf7c9
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-14 09:45:59 +0530

    Every and some

---
+++ JAVASCRIPT/PART 8/Every&Some.js
@@ -0,0 +1,6 @@
+// EVERY
+// Returns true if every element of array gives true for some function. Else returns false.
+// arr.every(some function defination or name);
+[1, 2, 3, 4].every((el) => (el % 2 == 0)); // logical "AND"
+
+[1, 3].some((el) => (el % 2 == 0));
--- JAVASCRIPT/PART 8/Filter.js
+++ JAVASCRIPT/PART 8/Filter.js
@@ -1,4 +1,6 @@
 // FILTER
 // let newArr = arr.filter(some function defination or name);
 let nums = [2, 4, 1, 5, 6, 2, 7, 8, 9];
-let even = nums.filter((num) => (num % 2 == 0));
+let even = nums.filter((num) => {
+    return num % 2 == 0 ;
+});
