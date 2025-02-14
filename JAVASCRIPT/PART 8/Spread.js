c718dc46a0f1317c2587b53d29938d3de1120e6a
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-14 10:57:22 +0530

    Spread

---
+++ JAVASCRIPT/PART 8/Spread.js
@@ -0,0 +1,9 @@
+// SPREAD
+// Expands an iterable into multiple values
+
+function func(...arr){
+    // do something
+}
+console.log(..."apnacollege");
+let nums = [12,23,34,45,67768,89,5,3,4,445,678,990];
+console.log(Math.min(...nums));
