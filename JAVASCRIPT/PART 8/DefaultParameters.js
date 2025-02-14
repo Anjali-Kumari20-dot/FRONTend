c2c8c84535e2fc89d10b3e27a1b029a1ffcd6efc
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-14 10:51:48 +0530

    Default parameters

---
+++ JAVASCRIPT/PART 8/DefaultParameters.js
@@ -0,0 +1,11 @@
+// DEFAULT PARAMETERS
+// Giving a default value to the arguments
+
+// function func (a, b = 2){
+//     // do something
+// }
+
+function sum (a, b = 3){
+    console.log(`${a} + ${b} = ${a+b}`);
+}
+sum(17562, 8948576);
