82d9af52c66402849c0fd65878f3fbbe59b4dcd8
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-19 15:36:42 +0530

    change event

--- JAVASCRIPT/PART 10/Form_Events.js
+++ JAVASCRIPT/PART 10/Form_Events.js
@@ -13,11 +13,13 @@ form.addEventListener("submit", function(event){
 
     // let user = document.querySelector("#user");
     // let password = document.querySelector("#pass");
+
+    let user = this.elements[0]; // form.elements[0]
+    let password = this.elements[1];
     
-    // console.log(user.value);
-    // console.log(password.value);
+    console.log(user.value);
+    console.log(password.value);
 
-    // alert(`Hi ${user.value} 😊, your password is set to ${password.value}`);
-
+    alert(`Hi ${user.value} 😊, your password is set to ${password.value}`);
 
 });
