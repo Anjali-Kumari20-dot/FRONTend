d9e9c75d533a43cb7add19266474f6d032f1d08a
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-22 14:41:52 +0530

    Visualsing the call stack

--- JAVASCRIPT/PART 11/app.js
+++ JAVASCRIPT/PART 11/app.js
@@ -1,14 +1,30 @@
-function hello() {
-    console.log("Inside hello function");
-    console.log("Hello");
+//------------ JS CALL STACK -------------
+// function hello() {
+//     console.log("Inside hello function");
+//     console.log("Hello");
+// }
+
+// function demo(){
+//     console.log("Calling hello function");
+//    hello(); 
+// }
+
+// console.log("Calling demo function");
+// demo();
+
+// console.log("Done, bye!");
+
+
+//-------- VISUALISING THE CALL STACK -------
+function one(){
+    return 1;
+}
+function two(){
+    return one() + one();
+}
+function three(){
+    let ans = two() + one();
+    console.log(ans);
 }
 
-function demo(){
-    console.log("Calling hello function");
-   hello(); 
-}
-
-console.log("Calling demo function");
-demo();
-
-console.log("Done, bye!");
+three();
