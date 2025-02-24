79711c51fb0fc837948a3b34840b0ae70c53be31
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-24 16:13:55 +0530

    Await keyword

--- JAVASCRIPT/PART 12/app.js
+++ JAVASCRIPT/PART 12/app.js
@@ -1,14 +1,62 @@
 //------------------- ASYNC FUNCTIONS ----------------------
-async function greet() {
-    throw "Weak connection";
-    return "hello world!" ; // returns a promise
+// async function greet() {
+//     throw "Weak connection";
+//     return "hello world!" ; // returns a promise
+// }
+
+// greet()
+// .then((result) => {
+//     console.log("Promise was resolved");
+//     console.log("Result was : ", result);
+// })
+// .catch((err) => {
+//     console.log("Promise was rejected with err : ", err);
+// })
+
+// let demo = async () => {
+//     return 5;
+// }
+
+
+//------------------- AWAIT KEYWORD ---------------------
+// pauses the execution of its surrounding async function until the promise is settlled (resolved or rejected)
+
+// function getNum () {
+//     return new Promise ((resolve, reject) => {
+//         setTimeout(() => {
+//             let num = Math.floor(Math.random() * 10) + 1;
+//             console.log(num);
+//             resolve();
+//         }, 1000);
+//     });
+// }
+
+// async function demo() {
+//     await getNum();
+//     await getNum();
+//     await getNum(); 
+//     await getNum(); 
+//     getNum();
+// }
+
+h1 = document.querySelector("h1");
+
+function colorChange(color, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            h1.style.color = color;
+            console.log(`Color changed to ${color}!`);
+            resolve("color changed");
+        }, delay);
+    });
 }
 
-greet()
-.then((result) => {
-    console.log("Promise was resolved");
-    console.log("Result was : ", result);
-})
-.catch((err) => {
-    console.log("Promise was rejected with err : ", err);
-})
+async function show() {
+    await colorChange("violet", 1000);
+    await colorChange("indigo", 1000);
+    await colorChange("green", 1000);
+    await colorChange("yellow", 1000);
+    await colorChange("orange", 1000);
+
+    return "done";
+}
