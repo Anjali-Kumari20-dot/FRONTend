c799823690f48bcbeac3bbc2b12d5be89acd20c3
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-25 12:20:48 +0530

    Accessing JSON data

---
+++ JAVASCRIPT/PART 12/API/app.js
@@ -0,0 +1,11 @@
+let jsonRes = '{"message":"https:\/\/images.dog.ceo\/breeds\/lhasa\/n02098413_17364.jpg","status":"success"}';
+
+let validRes = JSON.parse(jsonRes);
+console.log(validRes);
+
+console.log(validRes.message);
+
+let student = {
+    name : "Anjali",
+    marks: 95
+};
