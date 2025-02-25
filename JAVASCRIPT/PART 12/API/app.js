25eabb61a7f2eb799b5dc1bc068c58aeb942bf47
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-25 14:19:41 +0530

    Using fetch with async-await

--- JAVASCRIPT/PART 12/API/app.js
+++ JAVASCRIPT/PART 12/API/app.js
@@ -10,22 +10,47 @@
 //     marks: 95 
 // };
 
+
+//---------------- OUR FIRST API REQUEST --------------- 
+
+// let url = "https://catfact.ninja/fact";
+// fetch(url)
+//     .then((res) => {
+//         console.log(res);
+//         return res.json();
+//     })
+//     .then((data) => {
+//         console.log("data1 = " ,data.fact);
+//         return fetch(url);
+//     })
+//     .then((res) => {
+//         return res.json();
+//     })
+//     .then((data2) => {
+//         console.log("data2 = ", data2.fact);
+//     })
+//     .catch((err) => {
+//         console.log("ERROR - ",err);
+//     });
+
+//     console.log("I am happy :)");
+
+//------------ USING FETCH WITH ASYNC-AWAIT ------------
+
 let url = "https://catfact.ninja/fact";
-fetch(url)
-    .then((res) => {
-        console.log(res);
-        return res.json();
-    })
-    .then((data) => {
-        console.log("data1 = " ,data.fact);
-        return fetch(url);
-    })
-    .then((res) => {
-        return res.json();
-    })
-    .then((data2) => {
-        console.log("data2 = ", data2.fact);
-    })
-    .catch((err) => {
-        console.log("ERROR - ",err);
-    });
+
+async function getFacts() {
+    try{
+        let res = await fetch(url);
+        let data = await res.json();
+        console.log(data.fact);
+
+        let res2 = await fetch(url);
+        let data2 = await res.json();
+        console.log(data2.fact);
+    }catch(e){
+        console.log("Error - ", e);
+    }
+
+    console.log("bye");
+}
