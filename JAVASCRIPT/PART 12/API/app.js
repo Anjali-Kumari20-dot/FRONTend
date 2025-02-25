22006ca4ad8f356250638e90499df0684ed87048
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-25 14:04:08 +0530

    Our first API request

--- JAVASCRIPT/PART 12/API/app.js
+++ JAVASCRIPT/PART 12/API/app.js
@@ -1,11 +1,31 @@
-let jsonRes = '{"message":"https:\/\/images.dog.ceo\/breeds\/lhasa\/n02098413_17364.jpg","status":"success"}';
+// let jsonRes = '{"message":"https:\/\/images.dog.ceo\/breeds\/lhasa\/n02098413_17364.jpg","status":"success"}';
 
-let validRes = JSON.parse(jsonRes);
-console.log(validRes);
+// let validRes = JSON.parse(jsonRes);
+// console.log(validRes);
 
-console.log(validRes.message);
+// console.log(validRes.message);
 
-let student = {
-    name : "Anjali",
-    marks: 95
-};
+// let student = {
+//     name : "Anjali",
+//     marks: 95 
+// };
+
+let url = "https://catfact.ninja/fact";
+fetch(url)
+    .then((res) => {
+        console.log(res);
+        return res.json();
+    })
+    .then((data) => {
+        console.log("data1 = " ,data.fact);
+        return fetch(url);
+    })
+    .then((res) => {
+        return res.json();
+    })
+    .then((data2) => {
+        console.log("data2 = ", data2.fact);
+    })
+    .catch((err) => {
+        console.log("ERROR - ",err);
+    });
