a9d65aafe162918ddc53e716acadbdde298ebdc8
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-02-26 09:52:22 +0530

    Activity using api strings

--- JAVASCRIPT/PART 13/app.js
+++ JAVASCRIPT/PART 13/app.js
@@ -1,14 +1,51 @@
-const url = "https://icanhazdadjoke.com/";
+//------------ ACTIVITY USING QUERY STRINGS --------------
+let url = "http://universities.hipolabs.com/search?name=";
+let btn = document.querySelector("button");
 
-async function getjokes(){
-    try {
-        const config = { headers: {Accept: "application/json"}};
-        let res = await axios.get(url, config);
-        console.log(res.data);
-    } catch (err) {
-        console.log(err);
+btn.addEventListener("click", async () => {
+    let country = document.querySelector("input").value;
+    console.log(country);
+    let colArr = await getColleges(country);
+    console.log(colArr);
+    show(colArr);
+})
+
+function show(colArr) {
+    let list = document.querySelector("#list");
+    list.innerText = "";
+    for(col of colArr){
+        console.log(col.name);
+
+        let li = document.createElement("li");
+        li.innerText = col.name;
+        list.appendChild(li);
     }
 }
+
+async function getColleges(country) {
+    try{
+        let res = await axios.get(url + country);
+        return res.data;
+    } catch(e) {
+        console.log("error : ", e);
+        return [] ;
+    }
+}
+
+
+//--------- SENDING HEADERS WITH API REQUESTS -----------
+
+// const url = "https://icanhazdadjoke.com/";
+
+// async function getjokes(){
+//     try {
+//         const config = { headers: {Accept: "application/json"}};
+//         let res = await axios.get(url, config);
+//         console.log(res.data);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
 
 
 // let btn = document.querySelector("button");
