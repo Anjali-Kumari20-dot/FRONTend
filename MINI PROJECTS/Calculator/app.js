0d5e2a72ef0c19fa6ae323ffeb394ffd8ef8b9b7
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-03-05 21:28:17 +0530

    In progress

--- MINI PROJECTS/Calculator/app.js
+++ MINI PROJECTS/Calculator/app.js
@@ -1,14 +1,35 @@
 const boxes = document.querySelectorAll(".boxes");
-const print_values = document.querySelector("#values") 
+const print_values = document.querySelector("#values")
+const result = document.querySelector("#result"); 
 let string = "";
+
+// let multiplybtn = document.querySelector(".mul");
+//     multiplybtn.addEventListener('click', () => {
+//     const currentVal = calculatorDisplay.innerText ;
+//     calculatorDisplay.innerText  = currentVal + '*';
+// });
 
 Array.from(boxes).forEach(box => {
     box.addEventListener('click', (e) => {
-        console.log(e);
-        print_values.classList.add("view");
-        string += e.target.innerText;
-        // print_values.value = string;
-        print_values.innerText = string;
+        boxes.classList.add("onClick");
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            result.innerText = string;
+        }
+        else if (e.target.innerHTML == 'X'){
+            string += '*';
+            print_values.innerHTML += 'x';
+        }
+        else if(e.target.innerHTML == 'C'){
+            string = "";
+            result.innerText = string;
+            print_values.innerText = string;
+        }
+        else{
+            print_values.classList.add("view");
+            string += e.target.innerText;
+            print_values.innerText = string;
+        }
     })
 }) 
 
