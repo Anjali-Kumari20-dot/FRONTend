1cb9ec57eb68b5213bb1cb681dd549fa4868e791
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-03-06 11:23:16 +0530

    Express.js

--- ExpressDir/index.js
+++ ExpressDir/index.js
@@ -14,15 +14,27 @@ app.listen(port, () => {
 //     res.send(code);
 // });
 
-app.get("/", (req, res) => {
-    res.send("Hello, I am root");
+// app.get("/", (req, res) => {
+//     res.send("Hello, I am root");
+// });
+// app.get("/mango", (req, res) => {
+//     res.send("You contacted mango path");
+// });
+// app.get("/banana", (req, res) => {
+//     res.send("You contacted banana path");
+// });
+// app.get("*", (req, res) => {
+//     res.send("This path does not exist");
+// });
+app.get("/:username/:id", (req, res) => {
+    let {username, id} = req.params;
+    let htmlStr = `<h1>Welcome to the page of @${username}</h1>`;
+    res.send(htmlStr);
 });
-app.get("/mango", (req, res) => {
-    res.send("You contacted mango path");
-});
-app.get("/banana", (req, res) => {
-    res.send("You contacted banana path");
-});
-app.get("*", (req, res) => {
-    res.send("This path does not exist");
+app.get("/search", (req, res) => {
+    let { q } = req.query;
+    if(!q){
+        console.log("No search query")
+    }
+    res.send(`Search results for query : ${q}`);
 })
