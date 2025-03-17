73175882cff7f640f38ccd939de8ee8ca072da6e
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-03-17 19:51:13 +0530

    mySQL

--- SQLCLASS/index.js
+++ SQLCLASS/index.js
@@ -1,42 +1,37 @@
-import * as z from 'zod'
-import { generateMock } from '@anatine/zod-mock';
+// import * as z from "zod";
+// import { generateMock } from "@anatine/zod-mock";
 
-const mysql = require('mysql2');
+// const mysql = require("mysql2");
+import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'test',
-    password: 'anjali@123..'
+  host: "localhost",
+  user: "root",
+  database: "delta_app",
+  password: "123anjali..",
 });
 
-let getRandomUser = () => {
-        const schema = z.object({
-            id: z.string().nonempty(),
-            name: z.string(),
-            email: z.string().email().optional(),
-            password: z.string().min(10).optional(),
-        });
-    return generateMock(schema);
+try {
+    connection.query("SHOW TABLES", (err, result) => {
+        if(err) throw err;
+        console.log(result);
+    });
+}catch(err) {
+    console.log(err);
 };
+    
 
-console.log(getRandomUser());
+// let getRandomUser = () => {
+//   const schema = z.object({
+//     id: z.string().nonempty(),
+//     name: z.string(),
+//     email: z.string().email().optional(),
+//     password: z.string().min(10).optional(),
+//   });
+//   return generateMock(schema);
+// };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(getRandomUser());
 
 // const schema = z.object({
 //       uid: z.string().nonempty(),
