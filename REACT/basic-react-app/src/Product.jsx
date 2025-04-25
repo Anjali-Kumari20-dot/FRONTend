5e059d1ed3107a30244ceaabbcd449c65f9939aa
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-04-25 14:32:25 +0530

    Conditionals

--- REACT/basic-react-app/src/Product.jsx
+++ REACT/basic-react-app/src/Product.jsx
@@ -1,12 +1,11 @@
 import "./Product.css"
-function Product({title, price, features, features2, description}) {
+function Product({title, price, features}) {
   return (
     <div className="Product">
       <h3>{title}</h3>
       <h4>Price : {price}</h4>
-      <h5>{description}</h5>
       <p>{features.map((feature) => <li>{feature}</li>)}</p>
-      {/* <p>{features2}</p> */}
+      {price > 30000 && <p>"Discount of 5%"</p>}
     </div>
   );
 }
--- REACT/basic-react-app/src/ProductTab.jsx
+++ REACT/basic-react-app/src/ProductTab.jsx
@@ -1,36 +1,30 @@
 import Product from "./Product.jsx";
 
 function ProductTab() {
-    // RENDERING ARRAYS
+  // RENDERING ARRAYS
   let options = ["Hi-tech", "Lightweight", "Portable"];
-//   let options = [
-//     <li>"Hi-tech"</li>,
-//     <li>"Lightweight"</li>,
-//     <li>"Portable"</li>,
-//   ];
+  //   let options = [
+  //     <li>"Hi-tech"</li>,
+  //     <li>"Lightweight"</li>,
+  //     <li>"Portable"</li>,
+  //   ];
   let options2 = { a: "Hi-tech", b: "Lightweight", c: "Portable" };
   return (
     <>
       <Product
         title="Phone"
-        price="30,000"
-        description="A mobile phone is a portable device that allows users to make calls, send texts, access the internet, and use various apps for communication, entertainment, and productivity."
+        price={30000}
         features={options}
-        // features2={options2.a}
       />
       <Product
         title="Laptop"
-        price="40,000"
-        description="A laptop is a portable personal computer designed for mobile use. It typically features a keyboard, touchpad, display screen, and can run various software applications for work, education, or entertainment."
+        price={40000}
         features={options}
-        // features2={options2.b}
       />
       <Product
         title="Printer"
-        price="50,000"
-        description=" A printer is a device that produces physical copies of digital documents or images. It can be connected to a computer or network, allowing users to print text, photos, and other files."
+        price={50000}
         features={options}
-        // features2={options2.c}
       />
     </>
   );
