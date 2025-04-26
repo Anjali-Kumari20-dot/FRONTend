7ad2ab3ee1450a82dff204f5d2ef592fbeb0e547
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-04-26 23:44:22 +0530

    State in react

---
+++ REACT/react-state-class/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Counter from "./Counter.jsx";
+
+function App() {
+  return (
+    <>
+      <h1>States in react</h1>
+      <Counter/>
+    </>
+  );
+}
+
+export default App;
