81df40108762b89851672333634e11c193d0dd53
Anjali-Kumari20-dot <kavitasingh95082@gmail.com>
2025-04-27 18:40:32 +0530

    State in React

--- REACT/react-state-class/src/Counter.jsx
+++ REACT/react-state-class/src/Counter.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react"
+
 export default function Counter(){
-    let count = 0;
-    function incCount() {
-        count += 1;
+    let [count, setCount] = useState(0);
+
+    let incCount = () => {
+        setCount(count + 1)
         console.log(count);
     }
     return (
